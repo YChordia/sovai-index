72b7f4816e3dd19ab679d969ee560c22d848ad00
YChordia <43012527+YChordia@users.noreply.github.com>
2025-11-08 17:42:57 -0800

    SovAI Added world map

--- ui-frontend/src/pages/Overview.tsx
+++ ui-frontend/src/pages/Overview.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react'
 import { CountrySummary, fetchCountries } from '../api'
 import Loading from '../components/Loading'
 import CountryTable from '../components/CountryTable'
-import MapPlaceholder from '../components/MapPlaceholder'
+import { useNavigate } from 'react-router-dom'
+import WorldChoroplethMap from '../components/WorldChoroplethMap'
 import TopBottomCards from '../components/TopBottomCards'
 
 export default function Overview() {
   const [data, setData] = useState<CountrySummary[] | null>(null)
   const [error, setError] = useState<string | null>(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetchCountries().then(setData).catch((e) => setError(String(e)))
@@ -23,11 +25,10 @@ export default function Overview() {
         <CountryTable data={data} />
       </Grid.Col>
       <Grid.Col span={{ base: 12, lg: 5 }}>
-        <MapPlaceholder />
+        <WorldChoroplethMap data={data} onCountryClick={(iso) => navigate(`/country/${iso}`)} />
         <div style={{ height: 16 }} />
         <TopBottomCards data={data} />
       </Grid.Col>
     </Grid>
   )
 }
-
